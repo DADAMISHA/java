a97f8cd012c7440f45a15e4b73c75efbe7090e44
Миша <92542042+DADAMISHA@users.noreply.github.com>
2021-12-28 22:50:16 +0300

    skript

---
+++ index.js
@@ -0,0 +1,31 @@
+/*console.log('Silence is golden');
+let v = 1+2-4;
+console.log(v);
+let a = 2;
+let b = 3;
+let d = a ** a;
+console.log (( a+4*b)*( a - 3 *b) + d );
+a = +prompt('первое число')
+b = +prompt('второе число')
+alert( a + b)
+a = +prompt('квадрат');
+alert(a**2)
+b = +prompt('куб')
+alert(b**3);*/
+/*let a = 2;
+c = prompt('введите число');
+if ( c > 3 ) {c = a + 10} else { c = a - 10};
+alert(c);*/
+let n = prompt('введите число');
+if(n < 7){
+    console.log('Yes')
+}
+if(n > 10){
+    console.log('No')
+}
+if(n == 9){
+    console.log('Eror')
+}
+
+
+
